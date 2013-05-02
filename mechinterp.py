e5032e203c4028e197653d250cf0bb8b69fe0cb3
Bryan Weber <bryan.weber@uconn.edu>
2013-05-02 16:19:05 -0400

    Modified mechinterp.py to have simpler logic. We now check for =, =>, or <=> on a line to define a reaction, rather than defining a reaction as the exclusion of a bunch of other conditions. The function now returns the line numbers of all of the reactions, the line numbers between each reaction, and the type of auxillary information (if any) associated with each reaction to the calling script. The code has been extensively commented to explain its purpose.

--- mechinterp.py
+++ mechinterp.py
@@ -1,20 +1,34 @@
-def mechinterp(infile, norxns):
+def mechinterp(filename, numRxns):
+    """Interpret CHEMKIN chemistry input files and return lists of line numbers and reaction info.
+
+    INPUT:
+    filename - string, mechanism input file
+    norxns - integer, number of reactions in the input mechanims
+    OUTPUT:
+    
+    """
+#
+#Begin function
+#
+    #
+    #Import the module for regular expressions
+    #
     import re
-    from decimal import Decimal
-
-    ##norxns = 1593
-
-    inputfile = open(infile,'r')
+    #
+    #Open, read, and close the input file, storing the lines of the
+    #input file in the list 'lines'.
+    #
+    inputfile = open(filename,'r')
     lines = inputfile.readlines()
     inputfile.close()
-##    outputfile = open('test.txt','w')
-    reactions = [x+1 for x in range(norxns)]
-    ##print(reactions)
-    reactionsmatch = re.compile(r'(?i)^\bREACTIONS\b|\bREAC\b')
-    newlinematch = re.compile(r'^\n')
-    commentmatch = re.compile(r'^!')
-    inlinecommatch = re.compile(r'.\!')
-    slashmatch = re.compile(r'/')
+    #
+    #Compile regular expressions for each of the expected keywords to be
+    #encountered. (?i) indicates ignore case.
+    #
+    startmatch = re.compile(r'(?i)^\bREACTIONS\b|\bREAC\b')
+    reactionmatch = re.compile(r'=|=>|<=>')
+    commentmatch = re.compile(r'^\!') # Match exclamation points at the beginning of the string
+    newlinematch = re.compile(r'^\n') # Match newlines if they're the first character in the string
     lowmatch = re.compile(r'(?i)^[\s]*LOW')
     highmatch = re.compile(r'(?i)^[\s]*HIGH')
     dupmatch = re.compile(r'(?i)\bDUP\b|\bDUPLICATE\b')
@@ -22,100 +36,115 @@ def mechinterp(infile, norxns):
     revmatch = re.compile(r'(?i)^[\s]*REV')
     plogmatch = re.compile(r'(?i)^[\s]*PLOG')
     chebmatch = re.compile(r'(?i)^[\s]*CHEB')
-    Amatch = re.compile(r'([-+]?[0-9]*\.[0-9]+([eE][-+]?[0-9]+)?)')
-    re.I
-    line=0
-    match = reactionsmatch.search(lines[line])
-    while not match:
-        line+=1
-        match = reactionsmatch.search(lines[line])
-    ##print(line+1,lines[line],end='')
-    startline = line+1
-    reaction = 0
-    reaclines = [0]*norxns
-    #line = inputfile.readline()
-    for work in lines[startline:]:
-        line+=1
-    ##    print(line)
-        skip1 = newlinematch.search(work)
-        skip2 = commentmatch.search(work)
-        skip3 = slashmatch.search(work)
-        skip4 = dupmatch.search(work)
-        skip5 = endmatch.search(work)
-        com = inlinecommatch.search(work)
-        if skip3 and not skip2 and com:
-    ##        print(skip3,skip2,com)
-            rever = work[::-1]
-    ##        print(line,rever)
-            comslash = re.search(r'/(?=.*\!)(?!.*/)',rever)
-            if comslash:
-                skip3 = False
-    ##            print(rever)
-    ##        else:
-    ##            print(rever)
-        if not skip1 and not skip2 and not skip3 and not skip4 and not skip5:
-    ##        lowline = line+1
-    ##        print(reaction)
-    ##        outputfile.write(str(reaction+1)+' '+work)
-            reaclines[reaction] = line
-            reaction += 1
-    ##        skip6 = lowmatch.search(lines[lowline])
-    ##        if skip6:
-    ##            print(line,lowline)
-    ##                Afactor = Amatch.search(lines[i])
-    ##                x = Decimal(Afactor.group())
-    ##                x = 2*x               
-    ##                modline = line[:Afactor.start()]+str(x)+line[Afactor.end():]
-    ##                print(modline,end='')
-    ##                print(work,end='')
-    ##                Afactor = Amatch.search(line)
-    ##                x = Decimal(Afactor.group())
-    ##                x = 2*x               
-    ##                modline = line[:Afactor.start()]+str(x)+line[Afactor.end():]
-    ##                print(modline,end='')
-        if skip5:
-            reaclines.insert(reaction+1,line)
-    ##        print(len(reaclines))
-    searchlines = [0]*norxns
-    extrainfo = [0]*norxns
-    for lstart in range(len(reaclines)-1):
-        if lstart > 2072:
-            print(lstart)
-        searchlines[lstart] = list(range(reaclines[lstart]+1,reaclines[lstart+1]))
-    ##    print(searchlines[lstart][:])
-        for lineno in searchlines[lstart][:]:
-            line = lines[lineno]
-    ##        print(line,end='')
-            skip1 = newlinematch.match(line)
-            skip2 = commentmatch.match(line)
-            if not skip1 and not skip2:
-    ##            print(lstart,line,end='')
-                skip6 = lowmatch.search(line)
-                skip7 = highmatch.search(line)
-                skip8 = revmatch.search(line)
-                skip9 = plogmatch.search(line)
-                skip10 = chebmatch.search(line)
-                if skip6:
-                    extrainfo[lstart:lstart+1] = [1]
-                elif skip7:
-                    extrainfo[lstart:lstart+1] = [2]
-                elif skip8:
-                    extrainfo[lstart:lstart+1] = [3]
-                elif skip9:
-                    extrainfo[lstart:lstart+1] = [4]
-                elif skip10:
-                    extrainfo[lstart:lstart+1] = [5]
-    ##                print(lstart,lineno,line,end='')
-    ##print(extrainfo)
-    ##for s in range(len(searchlines)):
-    ##    item = str(searchlines[s])+'\n'
-    ##    outputfile.write(item)
-    ##for number in range(norxns):
-    ##    if extrainfo[number]:
-    ##        print(number,'Extra Info',extrainfo[number])
-
-    ##print(searchlines)
-
-    ##print(lines[reaclines[0]])      
-    ##print(reaction)
-##    outputfile.close()
+    #
+    #Initialize 'reactionNum', a counter of the number of reactions, and
+    #'reacLines', a zero-based list of the line numbers of the reactions
+    #in the input file. Set the 'numRxns' element of the 'reacLines' list
+    #to the number of lines in the input file so that it can be used as
+    #a search parameter later.
+    #
+    reactionNum = 0
+    reacLines = [0]*numRxns
+    reacLines.insert(numRxns,len(lines))
+    #
+    #Begin a loop over all of the lines in the input file. The lines are
+    #stored in the variable 'line' for each iteration.
+    #
+    for lineNum in range(len(lines)):
+        line = lines[lineNum]
+        #
+        #Check the line for being a comment  or one of the three ways
+        #to define the a reaction
+        #
+        rxncond = reactionmatch.search(line)
+        comcond = commentmatch.search(line)
+        #
+        #If the reaction condition contains information and the comment
+        #condition contains no information, the line is a reaction. Put
+        #the line number of this reaction in the 'reacLines' list, and
+        #increment the reaction counter. Remember that since Python is
+        #zero-based, the reaction number of a reaction will be one more
+        #than the number from this loop
+        #
+        if rxncond is not None and comcond is None:
+            reacLines[reactionNum] = lineNum
+            reactionNum += 1
+        #
+        #Check if the end line has been reached. If it has, add the line
+        #number to the end of the reaclines list.
+        #
+    #
+    #End loop
+    #
+    #
+    #Initialize two lists to hold information about the reactions.
+    #'searchLines' is a list of lists of the line numbers between each
+    #reaction. 'extraInfo' is a list of integers corresponding to each
+    #type of reaction rate modification.
+    #
+    searchLines = [0]*numRxns
+    extraInfo = [0]*numRxns
+    #
+    #Begin loop to find and read all of the lines between each reaction
+    #to check for auxilliary information.
+    #
+    for i in range(len(reacLines)-1):
+        #
+        #Fill the ith element of 'searchLines' with a list of lines
+        #in the input file between the line number in the ith element
+        #of 'reacLines' and the line number in the (i+1)th element. Add
+        #1 to the first line number to avoid the reaction line itself.
+        #The 'range' function automatically excludes the last number in
+        #the range.
+        #
+        searchLines[i] = list(range(reacLines[i]+1,reacLines[i+1]))
+        #
+        #Loop over the line numbers in the ith element of 'searchLines'
+        #to look for auxillary information.
+        #
+        for lineNum in searchLines[i]:
+            line = lines[lineNum]
+            #
+            #Check if the line is a comment or blank
+            #
+            blankcond = newlinematch.match(line)
+            comcond = commentmatch.match(line)
+            if blankcond is None and comcond is None:
+                #
+                #Use an if/elif block to check whether the current line
+                #contains any auxillary information. The options 'LOW',
+                #'HIGH', 'REV', 'PLOG', and 'CHEB' are mutually
+                #exclusive, so there should be no chance of a different
+                #type overwriting a previous type.
+                #
+                lowcond = lowmatch.search(line)
+                highcond = highmatch.search(line)
+                revcond = revmatch.search(line)
+                plogcond = plogmatch.search(line)
+                chebcond = chebmatch.search(line)
+                if lowcond is not None:
+                    extraInfo[i:i+1] = [1]
+                elif highcond is not None:
+                    extraInfo[i:i+1] = [2]
+                elif revcond is not None:
+                    extraInfo[i:i+1] = [3]
+                elif plogcond is not None:
+                    extraInfo[i:i+1] = [4]
+                elif chebcond is not None:
+                    extraInfo[i:i+1] = [5]
+                #
+                #End if/elif
+                #
+            #
+            #End if
+            #
+        #
+        #End loop
+        #
+    #
+    #End Loop
+    #
+    return (reacLines, searchLines, extraInfo)
+#
+#End function
+#
